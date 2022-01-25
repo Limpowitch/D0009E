e3aff4aaf874ca530ceffee8f0e9f868772200d3
Tim Rosendahl <timrosendahl2@gmail.com>
2022-01-25 23:42:07 +0100

    uppg 0 fixad

--- Lab_4_uppg_1.py
+++ Lab_4_uppg_1.py
@@ -1,11 +1,51 @@
-class M:
-    def __init__(self) -> None:
-        pass
-    def K():
-        pass
+class Board:
+    def __init__(self,msg):
+        self.sugmin = [msg] 
+        
+    
+    def addmessage(self):
+        message = input("add message")
+        self.sugmin.append(message)
+        
 
 
 def main():
-    var = M()
-    var.K()
-    pass
+    Board_1 = Board("XOXO")
+    Board_2 = Board("A spectre is haunting europe")
+    kim = Board_1
+    chris = Board_2
+    while(True):
+        print("=== Bulletin Board System ===")
+        print("Kim reads message:", kim.sugmin)
+        print("Chris reads message:", chris.sugmin)
+        print("1: Direct Kim to other board")
+        print("2: Direct Chris to other board")
+        print("3: Tell Kim to post a message")
+        print("4: Tell Chris to post a message")
+        print("0: exit")
+        print("Enter choice:")
+        var = None
+        try:
+            var = int(input())
+        except:
+            print("Not a valid choice")
+        if var == 1:
+            if kim == Board_1:
+                kim = Board_2
+            else:
+                kim = Board_1
+        if var == 2:
+            if chris == Board_2:
+                chris = Board_1
+            else:
+                chris = Board_2
+        if var == 3:
+            Board_1.addmessage()
+                
+        if var == 4:
+            Board_2.addmessage()
+                
+        if var == 0:
+            break
+
+main()
